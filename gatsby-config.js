371a1b8d5259f9990943c2cc5b52a85d4d8bb754
Alex Sanders <a@snd.rs>
2019-10-02 14:48:43 +0100

    basic project

---
+++ gatsby-config.js
@@ -0,0 +1,55 @@
+const siteMetadata = {
+	title: 'Synchronize bookmarks!',
+	strapline: 'Cross-browser bookmark synching.',
+	description: `Mighty Marks keeps your browser bookmarks in sync – in Chrome, Firefox, Edge, Opera, Brave and more (Safari coming soon).`,
+	siteUrl: `https://mightymarks.app`,
+	author: `@sndrs`,
+	twitter: '@mightymarksapp',
+}
+
+module.exports = {
+	siteMetadata,
+	plugins: [
+		{
+			resolve: `gatsby-plugin-canonical-urls`,
+			options: {
+				siteUrl: siteMetadata.siteUrl,
+				stripQueryString: true,
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-robots-txt',
+			options: {
+				policy: [{ userAgent: '*', allow: '/' }],
+			},
+		},
+		{
+			resolve: `gatsby-plugin-sitemap`,
+			options: {
+				exclude: ['/bookmarks'],
+			},
+		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: 'UA-140924415-2',
+				head: false,
+				anonymize: true,
+				respectDNT: true,
+				pageTransitionDelay: 0,
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+			options: {
+				analyzerMode: 'static',
+				openAnalyzer: false,
+				production: true,
+				disable: process.env.NODE_ENV !== 'production',
+			},
+		},
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-typescript',
+		`gatsby-plugin-mdx`,
+	],
+}
