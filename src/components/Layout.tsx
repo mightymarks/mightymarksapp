c4b1672aab3ef6d56f8260dc5483f77efcd013be
Alex Sanders <a@snd.rs>
2019-10-08 01:05:34 +0100

    check in fonts and preload

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import 'typeface-roboto-mono'
-import CSSReset from './CSSReset'
+import { Helmet } from 'react-helmet'
+import '../fonts.css'
+import '../reset.css'
 import SEO from './SEO'
 
 const Layout = ({ children, ...props }) => (
-	<CSSReset>
+	<>
+		<Helmet>
+			<link
+				rel="preload"
+				href="/static/roboto-mono-v7-latin-regular-0c94e034ca06357576c2d03d623e1fcd.woff2"
+				as="font"
+			/>
+		</Helmet>
 		<SEO />
 		{children}
-	</CSSReset>
+	</>
 )
 
 export default Layout
