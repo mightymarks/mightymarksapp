7248c3b62ca00764195bffb285377a2267e85159
Alex Sanders <a@snd.rs>
2019-10-08 01:07:51 +0100

    use actual asset, not hardcoded name

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import '../fonts.css'
+import woff2 from '../fonts/roboto-mono-v7-latin-regular.woff2'
 import '../reset.css'
 import SEO from './SEO'
 
 const Layout = ({ children, ...props }) => (
 	<>
 		<Helmet>
-			<link
-				rel="preload"
-				href="/static/roboto-mono-v7-latin-regular-0c94e034ca06357576c2d03d623e1fcd.woff2"
-				as="font"
-			/>
+			<link rel="preload" href={woff2} as="font" />
 		</Helmet>
 		<SEO />
 		{children}
--- types.d.ts
+++ types.d.ts
@@ -2,6 +2,10 @@ declare module '*.png' {
 	export default string
 }
 
+declare module '*.woff2' {
+	export default string
+}
+
 declare module '*.svg' {
 	let SVG: (props: React.SVGProps<SVGSVGElement>) => JSX.Element
 	export default SVG
