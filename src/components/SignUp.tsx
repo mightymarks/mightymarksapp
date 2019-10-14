17ddc35ea11ffca601ea5864e75146a621199881
Alex Sanders <a@snd.rs>
2019-10-14 17:22:26 +0100

    fix up

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,12 +1,12 @@
 import { css } from 'linaria'
 import { styled } from 'linaria/react'
-import React from 'react'
-import { CSSTransition } from 'react-transition-group'
+import React, { useState } from 'react'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
 import { CSSTransitionProps } from 'react-transition-group/CSSTransition'
 import { tablet } from '../mq'
 import { colors, fonts, fontSizes } from '../theme'
 import Button from './Button'
-import { signIn, useAuthState } from './Firebase'
+import { signIn, signOut, useAuthState } from './Firebase'
 import GitHubIcon from './GitHubIcon'
 
 const TRANSITION_DURATION = 200
@@ -15,33 +15,21 @@ const iconStyles = css`
 	margin-left: 1ch;
 `
 
-const User = styled.div`
-	margin: 10vw 0 2vw;
-	font-family: ${fonts.sans};
-	font-size: ${fontSizes[1]};
-	position: relative;
-	height: 2rem;
-
-	${tablet} {
-		margin-top: 6vw;
-	}
-`
-
 const transitionCSS = css`
 	transition: opacity ${TRANSITION_DURATION}ms;
-	position: absolute;
 
-	&.enter {
+	&.enter,
+	&.appear {
 		opacity: 0;
 	}
 
-	&.enter-active {
+	&.enter-active,
+	&.appear-active {
 		opacity: 1;
 	}
 
 	&.exit {
 		opacity: 1;
-		transition: opacity 0ms;
 	}
 
 	&.exit-active {
@@ -49,52 +37,75 @@ const transitionCSS = css`
 	}
 `
 
-const Error: React.FC<{ error: firebase.auth.Error }> = ({ error }) => (
-	<p>Error: {error}</p>
-)
+const User: React.FC<Partial<CSSTransitionProps>> = styled.div`
+	margin: 10vw 0 2vw;
 
-const SignOut = styled.div`
-	color: ${colors.grey[8]};
+	${tablet} {
+		margin-top: 6vw;
+	}
+`
+
+const SignedIn = styled.div`
+	font-family: ${fonts.sans};
+	font-size: ${fontSizes[1]};
+	color: ${colors.black};
 
 	em {
 		font-style: normal;
 		font-weight: 700;
 	}
 
-	p + p {
-		margin-top: 1rem;
+	p {
+		& + p {
+			margin-top: 1rem;
+		}
 	}
 `
 
-const Transition: React.FC<Partial<CSSTransitionProps>> = props => (
-	<CSSTransition
-		className={transitionCSS}
-		timeout={TRANSITION_DURATION}
-		unmountOnExit
-		{...props}
-	/>
-)
+const Error = styled.p`
+	color: ${colors.red};
+`
 
 const SignUp: React.FC = () => {
 	const [user, loading, error] = useAuthState()
 
+	if (error)
+		return (
+			<User>
+				<Error>{error}</Error>
+			</User>
+		)
+
 	return (
-		<User>
-			{error && <Error error={error} />}
-			<Transition in={!loading && !user}>
-				<Button onClick={signIn} color={colors.blue} className={transitionCSS}>
-					Join the waiting list with GitHub
-					<GitHubIcon className={iconStyles} />
-				</Button>
-			</Transition>
-			<Transition in={!loading && !!user}>
-				<SignOut className={transitionCSS}>
-					<p>
-						You are on the waiting list as <em>{user && user.email}</em>.
-					</p>
-				</SignOut>
-			</Transition>
-		</User>
+		!loading && (
+			<User>
+				<SwitchTransition>
+					<CSSTransition
+						className={transitionCSS}
+						timeout={TRANSITION_DURATION}
+						key={user ? 'hasUser' : 'noUser'}
+						appear
+					>
+						{user ? (
+							<SignedIn>
+								<p>
+									You are on the waiting list as <em>{user && user.email}</em>.
+								</p>
+								<br />
+								<Button onClick={signOut} color={colors.blue} hollow small>
+									Sign out
+								</Button>
+							</SignedIn>
+						) : (
+							<Button onClick={signIn} color={colors.blue}>
+								Join the waiting list
+								<GitHubIcon className={iconStyles} />
+							</Button>
+						)}
+					</CSSTransition>
+				</SwitchTransition>
+			</User>
+		)
 	)
 }
 
