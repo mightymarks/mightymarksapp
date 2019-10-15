89fb5802601d4dcc8f253c1d2023bbe6838c58ce
Alex Sanders <a@snd.rs>
2019-10-15 13:07:08 +0100

    pffffff

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -1,22 +1,22 @@
 /// <reference types="Cypress" />
 
-describe('The Home Page', function() {
+describe('Home Page', function() {
 	beforeEach(() => indexedDB.deleteDatabase('firebaseLocalStorageDb'))
 	beforeEach(() => cy.visit('/'))
 
-	it('shows the cookie banner', () =>
-		cy
-			.get('h4')
+	it('shows the cookie banner', () => {
+		cy.get('h4')
 			.contains('Let’s get this out of the way…')
 			.parent()
 			.then($el => {
-				expect(Cypress.dom.isHidden($el)).to.be.false
+				expect($el).to.be.visible
 				cy.get('button', { withinSubject: $el })
 					.click()
 					.then(() => {
-						expect(Cypress.dom.isHidden($el)).to.be.true
+						expect($el).not.to.exist
 					})
-			}))
+			})
+	})
 
 	it('shows the sign up button', () => cy.contains('Join the waiting list'))
 
