65a29c892bdbd6fc17afdf7ed936f7873250a3d9
Alex Sanders <a@snd.rs>
2019-10-15 18:20:29 +0100

    does this break?

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,64 +1,15 @@
-import { css } from 'linaria'
 import { styled } from 'linaria/react'
 import React from 'react'
-import { CSSTransition, SwitchTransition } from 'react-transition-group'
 import { CSSTransitionProps } from 'react-transition-group/CSSTransition'
 import { tablet } from '../mq'
-import { colors, fonts, fontSizes } from '../theme'
-import Button from './Button'
-import { signIn, signOut, useAuthState } from './Firebase'
-import GitHubIcon from './GitHubIcon'
-
-const TRANSITION_DURATION = 200
-
-const iconStyles = css`
-	margin-left: 1ch;
-`
-
-const transitionCSS = css`
-	transition: opacity ${TRANSITION_DURATION}ms;
-
-	&.enter,
-	&.appear {
-		opacity: 0;
-	}
-
-	&.enter-active,
-	&.appear-active {
-		opacity: 1;
-	}
-
-	&.exit {
-		opacity: 1;
-	}
-
-	&.exit-active {
-		opacity: 0;
-	}
-`
+import { colors } from '../theme'
+import { useAuthState } from './Firebase'
 
 const User: React.FC<Partial<CSSTransitionProps>> = styled.div`
 	margin: 10vw 0 2vw;
 
 	${tablet} {
 		margin-top: 6vw;
-	}
-`
-
-const SignedIn = styled.p`
-	font-family: ${fonts.sans};
-	font-size: ${fontSizes[1]};
-	color: ${colors.grey[7]};
-
-	em {
-		font-style: normal;
-		font-weight: 700;
-	}
-
-	p {
-		& + p {
-			margin-top: 1rem;
-		}
 	}
 `
 
