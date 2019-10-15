a4987d10c017fa4f741d26a2af0e995c79d428ec
Alex Sanders <a@snd.rs>
2019-10-15 12:38:29 +0100

    nowa it?

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -4,21 +4,19 @@ describe('The Home Page', function() {
 	it('successfully loads', function() {
 		cy.visit('/')
 
-		cy.wait(60000).then(() => {
-			cy.contains('Join the waiting list')
+		cy.contains('Join the waiting list')
 
-			cy.get('h4')
-				.contains('Let’s get this out of the way…')
-				.parent()
-				.then($el => {
-					expect(Cypress.dom.isHidden($el)).to.be.false
-					cy.get('button', { withinSubject: $el })
-						.click()
-						.then(() => {
-							expect(Cypress.dom.isHidden($el)).to.be.true
-						})
-				})
-		})
+		cy.get('h4')
+			.contains('Let’s get this out of the way…')
+			.parent()
+			.then($el => {
+				expect(Cypress.dom.isHidden($el)).to.be.false
+				cy.get('button', { withinSubject: $el })
+					.click()
+					.then(() => {
+						expect(Cypress.dom.isHidden($el)).to.be.true
+					})
+			})
 
 		cy.percySnapshot()
 	})
