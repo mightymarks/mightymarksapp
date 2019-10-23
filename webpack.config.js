52cc2554a17d5658236119522dd047a08dd63c09
Alex Sanders <a@snd.rs>
2019-10-23 11:16:14 +0100

    update extension build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 
+const CopyPlugin = require('copy-webpack-plugin')
 const WebpackExtensionManifestPlugin = require('webpack-extension-manifest-plugin')
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 const ExtensionReloader = require('webpack-extension-reloader')
@@ -8,14 +9,16 @@ const TerserPlugin = require('terser-webpack-plugin')
 
 const DEV = process.env.NODE_ENV === 'development'
 
+const DIST = path.resolve(__dirname, 'dist')
+
 module.exports = {
 	context: path.resolve(__dirname, 'src', 'extension'),
 	entry: {
 		background: './index.ts',
 	},
 	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name]/index.js',
+		path: DIST,
+		filename: '[name].js',
 	},
 	module: {
 		rules: [
@@ -40,6 +43,12 @@ module.exports = {
 		new WebpackExtensionManifestPlugin({
 			config: { base: require('./src/extension/manifest.js') },
 		}),
+		new CopyPlugin([
+			{
+				from: require.resolve('webextension-polyfill'),
+				to: DIST,
+			},
+		]),
 		DEV
 			? null
 			: new BundleAnalyzerPlugin({
